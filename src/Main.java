1f73c211beef453cec7c36a20869b8aadb7128bf
xposj800 <xposj800@gmail.com>
2021-06-01 11:45:46 +0200

    c2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
+        String name = "Jarda";
+
+        cheer (name);
+
     }
 
-    public static void cheer(){
-        System.out.println ("Hello Java team");
+    public static void cheer(String name){
+
+        System.out.println ("Hello Java team member, " + name);
     }
 }
